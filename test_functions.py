3627c1f9f345c24a25beb01f76ad1346017949bb
Agustin Alonso <alonsoiagustin@gmail.com>
2024-07-02 23:41:11 +0200

    test: :test_tube: Run tests for separate_into_units function

---
+++ test_functions.py
@@ -0,0 +1,7 @@
+from functions import separate_into_units
+
+def test_separate_into_units():
+    assert separate_into_units(1) == [1]
+    assert separate_into_units(11) == [10,1]
+    assert separate_into_units(111) == [100,10,1]
+    assert separate_into_units(1111) == [1000,100,10,1]
