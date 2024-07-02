7a2982d0af428bda88cd08a9baf2a57e6bea88de
Agustin Alonso <alonsoiagustin@gmail.com>
2024-07-03 00:28:39 +0200

    test: :test_tube: Create test for to_roman function

--- test_functions.py
+++ test_functions.py
@@ -1,4 +1,4 @@
-from functions import separate_into_units
+from functions import separate_into_units, to_roman
 
 def test_separate_into_units():
     assert separate_into_units(1) == [1]
@@ -6,7 +6,20 @@ def test_separate_into_units():
     assert separate_into_units(111) == [100,10,1]
     assert separate_into_units(1111) == [1000,100,10,1]
 
+def test_to_roman():
+    assert to_roman(1) == "I"
+    assert to_roman(2) == "II"
+    assert to_roman(3) == "III"
 
+    assert to_roman(4) == "IV"
+
+    assert to_roman(5) == "V"
+    assert to_roman(6) == "VI"
+    assert to_roman(7) == "VII"
+    assert to_roman(8) == "VIII"
+
+    assert to_roman(9) == "IX"
+    assert to_roman(10) == "X"
 
 
 
